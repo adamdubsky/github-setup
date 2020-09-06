0b349e282dc33412724c4f253bd26827a9bb005f
Adam Dubsky <ad547919@ohio.edu>
2020-09-06 09:52:34 -0400

    Enter a message here

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Adam Dubsky>
+ *        Date: <09/02/2020>
+ * Description: Lab 2 step 15
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+
+    cout << "Hello Git!"<< endl;
+    return 0;
+}// main
